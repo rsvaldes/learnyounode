72da5435d4eea0a53ad0ac4df8ac2c30aa118eca
YOUR FULL NAME <rachaelsvaldes@gmail.com>
2017-05-24 09:56:40 -0600

    three tests passing:

--- program.js
+++ program.js
@@ -1,7 +1,6 @@
-// console.log('HELLO WORLD');
-var arr = process.argv;
-var sum = 0;
-for (var i=2;i<arr.length;i++) {
-  sum += parseInt(arr[i]);
-  }
-  console.log(sum);
+var fs = require('fs');
+function count (text) {
+  var str = text.toString().split('\n').length - 1
+  console.log(str);
+}
+count(fs.readFileSync(process.argv[2]));
